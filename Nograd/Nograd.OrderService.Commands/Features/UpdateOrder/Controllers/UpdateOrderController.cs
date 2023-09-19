f048e01eedc1ac429810b9279cfe52c8429e402f
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-09-19 21:40:11 +0200

    same for update

--- Nograd/Nograd.OrderService.Commands/Features/UpdateOrder/Controllers/UpdateOrderController.cs
+++ Nograd/Nograd.OrderService.Commands/Features/UpdateOrder/Controllers/UpdateOrderController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Nograd.OrderService.Commands.Features.UpdateOrder.Mappers;
+using Nograd.ProductService.Queries.Client;
 
 namespace Nograd.OrderService.Commands.Features.UpdateOrder.Controllers;
 
@@ -11,15 +12,18 @@ public sealed class UpdateOrderController : ControllerBase
     private readonly IMediator _mediator;
     private readonly IUpdateOrderControllerInputToCommandMapper _mapper;
     private readonly ILogger<UpdateOrderController> _logger;
+    private readonly IProductQueriesClient _productQueriesClient;
 
     public UpdateOrderController(
         IMediator mediator,
         ILogger<UpdateOrderController> logger,
-        IUpdateOrderControllerInputToCommandMapper mapper)
+        IUpdateOrderControllerInputToCommandMapper mapper,
+        IProductQueriesClient productQueriesClient)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _productQueriesClient = productQueriesClient ?? throw new ArgumentNullException(nameof(productQueriesClient));
     }
 
     [HttpPut]
@@ -28,6 +32,17 @@ public sealed class UpdateOrderController : ControllerBase
         try
         {
             var command = _mapper.Map(input);
+
+            foreach (var productQuantity in command.ProductQuantities)
+            {
+                var productId = productQuantity.ProductId;
+                var foundProduct = await _productQueriesClient.GetProductByIdOrDefaultAsync(productId);
+                if (foundProduct == null)
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        new UpdateOrderControllerOutput(input.Id,
+                            $"Can't update order because product with id {productId} not found or not exists"));
+            }
+
             await _mediator.Send(command);
             return StatusCode(StatusCodes.Status202Accepted,
                 new UpdateOrderControllerOutput(command.OrderId, "Update order request completed successfully!"));

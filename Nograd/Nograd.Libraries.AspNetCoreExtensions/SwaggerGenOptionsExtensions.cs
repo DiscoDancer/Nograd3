12ec4ebc1fb3882b1578e85dc2fdba00c57d0fe4
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-09-20 12:57:54 +0200

    hide endpoints from other assemblies in swagger

---
+++ Nograd/Nograd.Libraries.AspNetCoreExtensions/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Nograd.Libraries.AspNetCoreExtensions;
+
+public static class SwaggerGenOptionsExtensions
+{
+    public static void HideEndpointsFromOtherAssemblies(this SwaggerGenOptions options, Type programType)
+    {
+        options.DocInclusionPredicate((_, apiDescription) =>
+        {
+            var fullMethodPath = apiDescription.ActionDescriptor.DisplayName;
+            var assemblyName = programType.Assembly.GetName().Name;
+
+            if (string.IsNullOrEmpty(fullMethodPath))
+                throw new Exception($"{nameof(fullMethodPath)} can't be empty");
+            if (string.IsNullOrEmpty(assemblyName)) throw new Exception($"{nameof(assemblyName)} can't be empty");
+
+            return fullMethodPath.Contains(assemblyName);
+        });
+    }
+}

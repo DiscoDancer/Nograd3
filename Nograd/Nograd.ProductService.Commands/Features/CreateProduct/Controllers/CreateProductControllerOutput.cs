9f9de46d62e4a244200c222e39e5fe7d312f7612
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-10-02 15:59:05 +0200

    created works fixed serialization issues

--- Nograd/Nograd.ProductService.Commands/Features/CreateProduct/Controllers/CreateProductControllerOutput.cs
+++ Nograd/Nograd.ProductService.Commands/Features/CreateProduct/Controllers/CreateProductControllerOutput.cs
@@ -1,19 +1,18 @@
-﻿namespace Nograd.ProductService.Commands.Features.CreateProduct.Controllers
+﻿namespace Nograd.ProductService.Commands.Features.CreateProduct.Controllers;
+
+[Serializable]
+public sealed class CreateProductControllerOutput
 {
-    public sealed class CreateProductControllerOutput
+    public CreateProductControllerOutput(Guid id, string message)
     {
-        public CreateProductControllerOutput(Guid id, string message)
-        {
-            if (string.IsNullOrWhiteSpace(message))
-            {
-                throw new ArgumentNullException(nameof(message));
-            }
+        if (string.IsNullOrWhiteSpace(message)) throw new ArgumentNullException(nameof(message));
 
-            ProductId = id;
-            Message = message;
-        }
-
-        public Guid ProductId { get; }
-        public string Message { get; }
+        ProductId = id;
+        Message = message;
     }
+
+    public CreateProductControllerOutput() {}
+
+    public Guid? ProductId { get; set; }
+    public string? Message { get; set; }
 }

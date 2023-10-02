0b2deb70d3cbca2b76ebcd5bca1078d8474c3abb
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-10-02 15:40:05 +0200

    expose create, update edited possible error message

--- Nograd/Nograd.ProductService.Commands.Client/IProductCommandsClient.cs
+++ Nograd/Nograd.ProductService.Commands.Client/IProductCommandsClient.cs
@@ -1,8 +1,12 @@
-﻿using Nograd.ProductService.Commands.Features.RemoveProduct.Controllers;
+﻿using Nograd.ProductService.Commands.Features.CreateProduct.Controllers;
+using Nograd.ProductService.Commands.Features.RemoveProduct.Controllers;
+using Nograd.ProductService.Commands.Features.UpdateProduct.Controllers;
 
 namespace Nograd.ProductService.Commands.Client;
 
 public interface IProductCommandsClient
 {
     Task<RemoveProductControllerOutput> RemoveProductAsync(RemoveProductControllerInput input);
-}
+    Task<CreateProductControllerOutput> CreateProductAsync(CreateProductControllerInput input);
+    Task<UpdateProductControllerOutput> UpdateProductAsync(UpdateProductControllerInput input);
+} 
--- Nograd/Nograd.ProductService.Commands.Client/ProductCommandsClient.cs
+++ Nograd/Nograd.ProductService.Commands.Client/ProductCommandsClient.cs
@@ -1,4 +1,6 @@
-﻿using Nograd.ProductService.Commands.Features.RemoveProduct.Controllers;
+﻿using Nograd.ProductService.Commands.Features.CreateProduct.Controllers;
+using Nograd.ProductService.Commands.Features.RemoveProduct.Controllers;
+using Nograd.ProductService.Commands.Features.UpdateProduct.Controllers;
 using RestSharp;
 
 namespace Nograd.ProductService.Commands.Client;
@@ -28,7 +30,45 @@ public sealed class ProductCommandsClient : IProductCommandsClient
 
         if (!response.IsSuccessStatusCode || response.Data == null)
         {
-            throw new Exception("Failed to RemoveProductAsync");
+            throw new Exception("Client failed to execute RemoveProductAsync request to controller.");
+        }
+
+        return response.Data;
+    }
+
+    public async Task<CreateProductControllerOutput> CreateProductAsync(CreateProductControllerInput input)
+    {
+        if (input == null) throw new ArgumentNullException(nameof(input));
+        var request =
+            new RestRequest(
+                $"/{CreateProductControllerRoutes.ControllerRoute}/{CreateProductControllerRoutes.ActionRoute}",
+                Method.Post);
+        request.AddBody(input);
+
+        var response = await _restClient.ExecuteAsync<CreateProductControllerOutput>(request);
+
+        if (!response.IsSuccessStatusCode || response.Data == null)
+        {
+            throw new Exception("Client failed to execute CreateProductAsync request to controller.");
+        }
+
+        return response.Data;
+    }
+
+    public async Task<UpdateProductControllerOutput> UpdateProductAsync(UpdateProductControllerInput input)
+    {
+        if (input == null) throw new ArgumentNullException(nameof(input));
+        var request =
+            new RestRequest(
+                $"/{UpdateProductControllerRoutes.ControllerRoute}/{UpdateProductControllerRoutes.ActionRoute}",
+                Method.Put);
+        request.AddBody(input);
+
+        var response = await _restClient.ExecuteAsync<UpdateProductControllerOutput>(request);
+
+        if (!response.IsSuccessStatusCode || response.Data == null)
+        {
+            throw new Exception("Client failed to execute UpdateProductAsync request to controller.");
         }
 
         return response.Data;
